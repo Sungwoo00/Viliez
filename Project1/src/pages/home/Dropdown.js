08b6865ab5c1cab2caf633e0475113b026081a60
Ju-MINJAE <juminjae98@gmail.com>
2023-11-22 16:11:01 +0900

    231122

---
+++ Project1/src/pages/home/Dropdown.js
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+
+import styles from './Dropdown.module.css';
+
+const Dropdown = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState('All Items');
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const handleOptionClick = (option) => {
+    setSelectedOption(option);
+    setIsDropdownOpen(false);
+  };
+
+  return (
+    <>
+      {' '}
+      <div className={styles.select} onClick={toggleDropdown}>
+        <span className={styles.selected}>{selectedOption}</span>
+        <div
+          className={`${styles.caret} ${
+            isDropdownOpen
+              ? `${styles.caretRotate} ${styles.caretAnimation}`
+              : ''
+          }`}
+        ></div>
+      </div>
+      <ul
+        className={`${styles.menu} ${
+          isDropdownOpen ? `${styles.menuOpen} ${styles.menuAnimation}` : ''
+        }`}
+      >
+        <li
+          onClick={() => handleOptionClick('All Items')}
+          className={selectedOption === 'All Items' ? styles.active : ''}
+        >
+          All Items
+        </li>
+        <li
+          onClick={() => handleOptionClick('Option 1')}
+          className={selectedOption === 'Option 1' ? styles.active : ''}
+        >
+          Option 1
+        </li>
+        <li
+          onClick={() => handleOptionClick('Option 2')}
+          className={selectedOption === 'Option 2' ? styles.active : ''}
+        >
+          Option 2
+        </li>
+        <li
+          onClick={() => handleOptionClick('Option 3')}
+          className={selectedOption === 'Option 3' ? styles.active : ''}
+        >
+          Option 3
+        </li>
+        <li
+          onClick={() => handleOptionClick('Option 4')}
+          className={selectedOption === 'Option 4' ? styles.active : ''}
+        >
+          Option 4
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default Dropdown;
--- Project1/src/pages/home/Home.js
+++ Project1/src/pages/home/Home.js
@@ -2,13 +2,14 @@ import HomeItemList from './HomeItemList';
 import styles from './Home.module.css';
 import useCollection from '../../hooks/useCollection';
 import SyncLoader from 'react-spinners/SyncLoader';
+import Dropdown from './Dropdown';
 
 const Home = () => {
   const { documents, error, isLoading } = useCollection('Sharemarket');
 
   return (
     <div className={styles.container}>
-      <h1>All Item</h1>
+      <Dropdown />
       {isLoading && (
         <div className={styles.loadingContainer}>
           <h2>물건을 가져오는 중..</h2>
