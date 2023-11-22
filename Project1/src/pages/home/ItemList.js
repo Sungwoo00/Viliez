7bfa2c68feae3ae29ede266c92fca98f36eee2e8
Sungwoo00 <dhdpdhdp5899@gmail.com>
2023-11-22 18:02:53 +0900

    ItemList
    아이템 리스트 수정 변경 오류 해결 후 코드 리팩터링

--- Project1/src/pages/home/ItemList.js
+++ Project1/src/pages/home/ItemList.js
@@ -1,21 +1,11 @@
 import React, { useState } from 'react';
 import useFirestore from '../../hooks/useFirestore';
-// import styles from './MyPage.module.css';
 import styles from './ItemList.module.css';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
 
 const ItemList = ({ items }) => {
   const { deleteDocument, updateDocument } = useFirestore('Sharemarket');
-
   const [editing, setEditing] = useState({});
-  const [updatedTitle, setUpdatedTitle] = useState('');
-  const [updatedCategory, setUpdatedCategory] = useState('');
-  const [updatedPrice, setUpdatedPrice] = useState('');
-  const [updatedEa, setUpdatedEa] = useState('');
-  const [updaterentuser, setUpdateRentUser] = useState('')
-  const [updaterentalperiod, setRentalPeriod] = useState('');
-  const [updatedDescription, setUpdatedDescription] = useState('');
+  const [updatedItems, setUpdatedItems] = useState({});
 
   const toggleEditing = (itemId) => {
     setEditing((prevEditing) => ({
@@ -24,140 +14,147 @@ const ItemList = ({ items }) => {
     }));
 
     if (!editing[itemId]) {
-      setUpdatedTitle(items.find((item) => item.id === itemId).title);
-      setUpdatedCategory(items.find((item) => item.id === itemId).Category);
-      setUpdatedPrice(items.find((item) => item.id === itemId).price);
-      setUpdatedEa(items.find((item) => item.id === itemId).ea);
-      setUpdatedDescription(items.find((item) => item.id === itemId).description);
-      setUpdateRentUser(items.find((item) => item.id === itemId).rentuser)
-      setRentalPeriod(items.find((item) => item.id === itemId).rentalperiod)
-    } else {
-      setUpdatedTitle('');
-      setUpdatedCategory('');
-      setUpdatedPrice('');
-      setUpdatedEa('');
-      setUpdatedDescription('');
-      setUpdateRentUser('');
-      setRentalPeriod('');
+      const item = items.find((item) => item.id === itemId);
+      setUpdatedItems((prevItems) => ({
+        ...prevItems,
+        [itemId]: { ...item },
+      }));
     }
+  };
+
+  const cancelEditing = (itemId) => {
+    setEditing((prevEditing) => ({
+      ...prevEditing,
+      [itemId]: false,
+    }));
+
+    setUpdatedItems((prevItems) => {
+      const newItems = { ...prevItems };
+      delete newItems[itemId];
+      return newItems;
+    });
+  };
+
+  const handleChange = (itemId, field, value) => {
+    setUpdatedItems((prevItems) => ({
+      ...prevItems,
+      [itemId]: { ...prevItems[itemId], [field]: value },
+    }));
+  };
+
+  const handleUpdate = (itemId) => {
+    updateDocument(itemId, updatedItems[itemId]);
+    setEditing((prevEditing) => ({
+      ...prevEditing,
+      [itemId]: false,
+    }));
   };
 
   return (
     <>
-      {items.map((item) => {
-        return (
-          <li key={item.id}>
-            {/* {item.displayName && <p>{item.displayName}님의 물건입니다</p>} */}
-            {editing[item.id] ? (
-              <>
-                <label htmlFor='editTitle'>제목 : </label>
-                <input
-                  id='editTitle'
-                  type='text'
-                  placeholder='새로운 제목'
-                  value={updatedTitle}
-                  onChange={(event) => setUpdatedTitle(event.target.value)}
-                />
-                <label>카테고리 : </label>
-                <select
-                  value={updatedCategory}
-                  onChange={(event) => setUpdatedCategory(event.target.value)}
-                >
-                  <option value='가전'>가전</option>
-                  <option value='여행'>여행</option>
-                  <option value='의류'>의류</option>
-                  <option value='취미'>취미</option>
-                </select>
-                <label htmlFor='editPrice'>가격 : </label>
-                <input
-                  id='editPrice'
-                  type='number'
-                  placeholder='새로운 가격'
-                  value={updatedPrice}
-                  onChange={(event) => setUpdatedPrice(event.target.value)}
-                  min='5000'
-                  step='1000'
-                />
-                <label htmlFor='editEa'>수량 : </label>
-                <input
-                  id='editEa'
-                  type='number'
-                  placeholder='새로운 수량'
-                  value={updatedEa}
-                  onChange={(event) => setUpdatedEa(event.target.value)}
-                  min='1'
-                  step='1'
-                />
-                <label htmlFor='editDescription'>설명 : </label>
-                <textarea
-                  id='editDescription'
-                  placeholder='새로운 설명'
-                  value={updatedDescription}
-                  onChange={(event) =>
-                    setUpdatedDescription(event.target.value)
-                  }
-                />
-              </>
-            ) : (
-              <>
-                {item.rentuser ?
-                  <p>이 물품은 {item.rentuser}님이 예약 중입니다.</p> :
-                  <p>이 물품을 예약한 사람이 없습니다.</p>
-                }
-                {item.rentalPeriod && (
-                  <p>
-                    대여 가능 기간: {item.rentalPeriod.startDate.toDate().toLocaleDateString('ko-KR') || '시작 날짜 정보 없음'}부터 {item.rentalPeriod.endDate.toDate().toLocaleDateString('ko-KR') || '종료 날짜 정보 없음'}까지
-                  </p>
-                )}
-                <strong className={styles.title}>{item.title}</strong>
-                {/* <p className={styles.Category}>{item.Category}</p> */}
-                <p className={styles.price}>
-                  {item.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-                  원
-                </p>
-                <p className={styles.ea}>{item.ea}개</p>
-                <p className={styles.description}>{item.description}</p>
-              </>
-            )}
-            <>
-              <button
-                className={styles.btn_edit}
-                type='button'
-                onClick={() => {
-                  if (editing[item.id]) {
-                    const updatedData = {
-                      title: updatedTitle || item.title,
-                      Category: updatedCategory || item.Category,
-                      price: updatedPrice || item.price,
-                      ea: updatedEa || item.ea,
-                      description: updatedDescription || item.description,
-                    };
-                    updateDocument(item.id, updatedData);
+      {items.map((item) => (
+        <li key={item.id}>
+          {editing[item.id] ? (
+            <EditItemForm
+              item={updatedItems[item.id]}
+              handleChange={(field, value) => handleChange(item.id, field, value)}
+              handleUpdate={() => handleUpdate(item.id)}
+              cancelEditing={() => cancelEditing(item.id)}
+            />
+          ) : (
+            <ViewItem
+              item={item}
+              startEditing={() => toggleEditing(item.id)}
+              deleteItem={() => deleteDocument(item.id)}
+            />
+          )}
+        </li>
+      ))}
+    </>
+  );
+};
 
-                    setUpdatedTitle('');
-                    setUpdatedCategory('');
-                    setUpdatedPrice('');
-                    setUpdatedEa('');
-                    setUpdatedDescription('');
-                  }
-                  toggleEditing(item.id);
-                }}
-              >
-                {editing[item.id] ? '확인' : '수정'}
-              </button>
-              <button
-                className={styles.btn_delete}
-                type='button'
-                onClick={() => {
-                  deleteDocument(item.id);
-                }}
-              >
-                삭제
-              </button>
-            </>
-          </li>
-        );
-      })}
+const EditItemForm = ({ item, handleChange, handleUpdate, cancelEditing }) => {
+  return (
+    <>
+      <label htmlFor='editTitle'>제목 : </label>
+      <input
+        id='editTitle'
+        type='text'
+        placeholder='새로운 제목'
+        value={item.title}
+        onChange={(e) => handleChange('title', e.target.value)}
+      />
+
+      <label>카테고리 : </label>
+      <select
+        value={item.category}
+        onChange={(e) => handleChange('category', e.target.value)}
+      >
+        <option value='가전'>가전</option>
+        <option value='여행'>여행</option>
+        <option value='의류'>의류</option>
+        <option value='취미'>취미</option>
+      </select>
+
+      <label htmlFor='editPrice'>가격: </label>
+      <input
+        id='editPrice'
+        type='number'
+        placeholder='새로운 가격'
+        value={item.price}
+        onChange={(e) => handleChange('price', e.target.value)}
+        min='5000'
+        step='1000'
+      />
+
+      <label htmlFor='editEa'>수량: </label>
+      <input
+        id='editEa'
+        type='number'
+        placeholder='새로운 수량'
+        value={item.ea}
+        onChange={(e) => handleChange('ea', e.target.value)}
+        min='1'
+        step='1'
+      />
+
+      <label htmlFor='editDescription'>설명: </label>
+      <textarea
+        id='editDescription'
+        placeholder='새로운 설명'
+        value={item.description}
+        onChange={(e) => handleChange('description', e.target.value)}
+      />
+
+      <button className={styles.btn_edit} onClick={handleUpdate}>확인</button>
+      <button className={styles.btn_delete} onClick={cancelEditing}>취소</button>
+    </>
+  );
+};
+const ViewItem = ({ item, startEditing, deleteItem }) => {
+  return (
+    <>
+      {item.rentuser ? (
+        <p>이 물품은 {item.rentuser}님이 예약 중입니다.</p>
+      ) : (
+        <p>현재 예약 중인 사람이 없습니다.</p>
+      )}
+      {item.rentalPeriod && (
+        <p>
+          대여 가능 기간 : 
+          {item.rentalPeriod.startDate.toDate().toLocaleDateString('ko-KR') || '시작 날짜 정보가 없습니다.'} ~ 
+          {item.rentalPeriod.endDate.toDate().toLocaleDateString('ko-KR') || '종료 날짜 정보가 없습니다.'}
+        </p>
+      )}
+      <strong className={styles.title}>{item.title}</strong>
+      <p className={styles.price}>
+        {item.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원
+      </p>
+      <p className={styles.ea}>{item.ea}개</p>
+      <p className={styles.description}>{item.description}</p>
+      <button className={styles.btn_edit} onClick={startEditing}>수정</button>
+      <button className={styles.btn_delete} onClick={deleteItem}>삭제</button>
     </>
   );
 };
