638eafaa29f7a246bb0c6e750d2d25808ce8dbe8
Sungwoo00 <dhdpdhdp5899@gmail.com>
2023-12-05 23:26:16 +0900

    ItemForm
    아이템 폼 제출 css 수정

--- Project1/src/pages/home/ItemForm.js
+++ Project1/src/pages/home/ItemForm.js
@@ -153,91 +153,109 @@ const ItemForm = ({ uid }) => {
       <form onSubmit={handleSubmit}>
         <fieldset>
           <legend>상품 등록</legend>
-          <ReactDatePicker
-            className={Datepicker.ReactDatePicker}
-            id='rentalperiod'
-            // locale='ko'
-            // selected={rentalPeriod.startDate}
-            startDate={rentalPeriod.startDate}
-            endDate={rentalPeriod.endDate}
-            selectsRange
-            shouldCloseOnSelect={false}
-            monthsShown={2}
-            minDate={new Date()}
-            dateFormat=' yyyy년 MM월 dd일(eee) '
-            onChange={handleDateChange}
-            open={openDatePicker}
-            onInputClick={() => setOpenDatePicker(true)}
-            locale={ko}
-            readOnly
-          />
-          <input
-            type='file'
-            id='imageInput'
-            accept='image/*'
-            onChange={handleImageChange}
-            style={{ display: 'none' }}
-          />
-          <label htmlFor='imageInput'>이미지 선택하기</label>
+          <ul className={styles.formList}>
+            <li className={styles.formItem}>
+              <ReactDatePicker
+                className={Datepicker.ReactDatePicker}
+                id='rentalperiod'
+                // locale='ko'
+                // selected={rentalPeriod.startDate}
+                startDate={rentalPeriod.startDate}
+                endDate={rentalPeriod.endDate}
+                selectsRange
+                shouldCloseOnSelect={false}
+                monthsShown={2}
+                minDate={new Date()}
+                dateFormat=' yyyy년 MM월 dd일(eee) '
+                onChange={handleDateChange}
+                open={openDatePicker}
+                onInputClick={() => setOpenDatePicker(true)}
+                locale={ko}
+                readOnly
+              />
 
-          {imagePreviewUrl && (
-            <img
-              src={imagePreviewUrl}
-              alt='Preview'
-              className={styles.imagePreview}
-            />
-          )}
-          <input
-            placeholder='제목'
-            id='tit'
-            type='text'
-            value={title}
-            required
-            onChange={handleData}
-          />
-          <div className={styles.category_price_ea}>
-            <select
-              id='category'
-              value={category}
-              onChange={handleData}
-              required
-            >
-              <option>카테고리</option>
-              <option value='가전'>가전</option>
-              <option value='여행'>여행</option>
-              <option value='의류'>의류</option>
-              <option value='취미'>취미</option>
-            </select>
-            <input
-              placeholder='가격'
-              id='price'
-              type='number'
-              value={price}
-              required
-              onChange={handleData}
-              min='5000'
-              step='1000'
-            />
-            <input
-              placeholder='수량'
-              id='ea'
-              type='number'
-              value={ea}
-              required
-              onChange={handleData}
-              min='1'
-              step='1'
-            />
-          </div>
-          <textarea
-            placeholder='대여하는 사람을 위해 자세히 적어주세요.'
-            id='txt'
-            type='text'
-            value={description}
-            required
-            onChange={handleData}
-          />
+            </li>
 
+            <li className={styles.formItem}>
+              <input
+                type='file'
+                id='imageInput'
+                accept='image/*'
+                onChange={handleImageChange}
+                style={{ display: 'none' }}
+              />
+              <label htmlFor='imageInput'>이미지 선택하기</label>
+
+              {
+                imagePreviewUrl && (
+                  <img
+                    src={imagePreviewUrl}
+                    alt='Preview'
+                    className={styles.imagePreview}
+                  />
+                )
+              }
+            </li>
+
+            <li className={styles.formItem}>
+              <input
+                placeholder='제목'
+                id='tit'
+                type='text'
+                value={title}
+                required
+                onChange={handleData}
+              />
+            </li>
+
+            <li className={styles.formItem}>
+              <select
+                id='category'
+                value={category}
+                onChange={handleData}
+                required
+              >
+                <option>카테고리</option>
+                <option value='가전'>가전</option>
+                <option value='여행'>여행</option>
+                <option value='의류'>의류</option>
+                <option value='취미'>취미</option>
+              </select>
+            </li>
+
+            <li className={styles.formItem}>
+              <input
+                placeholder='가격'
+                id='price'
+                type='number'
+                value={price}
+                required
+                onChange={handleData}
+              />
+            </li>
+
+            <li className={styles.formItem}>
+              <input
+                placeholder='수량'
+                id='ea'
+                type='number'
+                value={ea}
+                required
+                onChange={handleData}
+              />
+            </li>
+
+            <li className={styles.formItem}>
+              <textarea
+                placeholder='대여하는 사람을 위해 자세히 적어주세요.'
+                id='txt'
+                type='text'
+                value={description}
+                required
+                onChange={handleData}
+              />
+            </li>
+          </ul>
           <button type='submit'>제출</button>
         </fieldset>
       </form>
@@ -246,3 +264,4 @@ const ItemForm = ({ uid }) => {
 };
 
 export default ItemForm;
+
