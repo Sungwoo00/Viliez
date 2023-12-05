b0a1641dadea025fe360ddf81351a81e3c0ba5c6
Sungwoo00 <dhdpdhdp5899@gmail.com>
2023-12-05 17:44:57 +0900

    RentedItemList css
    대여 아이템 리스트 css 수정 및 아이템 리스트 표시 항목 수정

--- Project1/src/pages/home/RentedItemList.js
+++ Project1/src/pages/home/RentedItemList.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import styles from './ItemList.module.css';
+import styles from './RentedItemList.module.css';
 
 const RentedItemList = ({ items, currentUserDisplayName }) => {
   return (
-    <>
+    <ol className={styles.list}>
       {items.filter(item => item.curRentInfo?.some(rentInfo => rentInfo.rentuser === currentUserDisplayName))
         .map((item) => {
           const userRentInfo = item.curRentInfo.find(
@@ -15,7 +15,7 @@ const RentedItemList = ({ items, currentUserDisplayName }) => {
             </li>
           );
         })}
-    </>
+    </ol>
   );
 };
 
@@ -33,18 +33,18 @@ const ViewItem = ({ item, rentInfo }) => {
   };
 
   return (
-    <>
+    <div className={styles.item}>
       {item.photoURL && <img src={item.photoURL} alt='Product' />}
       <strong className={styles.title}>{item.title}</strong>
       <p className={styles.price}>
         가격 : {formatPrice(item.price)}원
-      </p>
+      </p>  
       <p className={styles.description}>{item.description}</p>
-      <p>Rent date: {formatDate(new Date(rentInfo.startDate))}</p>
-      <p>End date: {formatDate(endDate)}</p>
-      <p>Default: {rentInfo.curRentEa}</p>
-      <strong>Remaining Days: {remainingDays}</strong>
-    </>
+      <p>대여 시작 날짜: {formatDate(new Date(rentInfo.startDate))}</p>
+      <p>대여 종료 날짜: {formatDate(endDate)}</p>
+      <p>빌린 수량: {rentInfo.curRentEa}</p>
+      <strong>반납 날까지 {remainingDays}일 남았습니다.</strong>
+    </div>
   );
 };
 
