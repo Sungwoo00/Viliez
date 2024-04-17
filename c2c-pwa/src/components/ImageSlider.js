c588c8d89329835f722794ff3a871573d6d2ba17
이성우 <iseong-u@iseong-uui-noteubug.local>
2024-04-17 23:13:25 +0900

    ImageSlider, HomeItemList
    이미지 슬라이더 컴포넌트 현재 이미지 표시 기능, 이미지 드래그로 넘기기 기능 및 css 수정

--- c2c-pwa/src/components/ImageSlider.js
+++ c2c-pwa/src/components/ImageSlider.js
@@ -3,34 +3,90 @@ import styles from "./ImageSlider.module.css";
 
 const ImageSlider = ({ photoURLs, onClick }) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [imageLoaded, setImageLoaded] = useState(false);
+  const [startPosition, setStartPosition] = useState(null);
 
   const handlePreviousImage = () => {
+    setImageLoaded(false);
     setCurrentImageIndex((prevIndex) =>
       prevIndex === 0 ? photoURLs.length - 1 : prevIndex - 1
     );
   };
+  const startDrag = (e) => {
+    e.preventDefault();
+    const position = e.clientX || e.touches[0].clientX;
+    setStartPosition(position);
+  };
+
+  const onDrag = (e) => {
+    if (startPosition === null) return;
+    const currentPos = e.touches ? e.touches[0].clientX : e.clientX;
+    const diff = startPosition - currentPos;
+    if (diff > 50) {
+      handleNextImage();
+      setStartPosition(null);
+    } else if (diff < -50) {
+      handlePreviousImage();
+      setStartPosition(null);
+    }
+  };
+
+  const endDrag = () => {
+    setStartPosition(null);
+  };
 
   const handleNextImage = () => {
+    setImageLoaded(false);
     setCurrentImageIndex((prevIndex) =>
       prevIndex === photoURLs.length - 1 ? 0 : prevIndex + 1
     );
   };
 
+  const renderIndicators = () => {
+    return photoURLs.map((_, idx) => (
+      <span
+        key={idx}
+        className={
+          idx === currentImageIndex ? styles.activeIndicator : styles.indicator
+        }
+      ></span>
+    ));
+  };
+
   return (
-    <div>
-      <button onClick={handlePreviousImage} className={styles.btn}>
-        이전
-      </button>
+    <div
+      onMouseDown={startDrag}
+      onMouseMove={onDrag}
+      onMouseUp={endDrag}
+      onMouseLeave={endDrag}
+      onTouchStart={startDrag}
+      onTouchMove={onDrag}
+      onTouchEnd={endDrag}
+    >
+      {imageLoaded && (
+        <button
+          onClick={handlePreviousImage}
+          className={`${styles.btn} ${styles.SliderBtn} ${styles.left}`}
+        >
+          &lt;
+        </button>
+      )}
       <img
         src={photoURLs[currentImageIndex]}
         alt={`Product ${currentImageIndex + 1}`}
-        // style={styles}
         className={styles.imgSlide}
         onClick={onClick}
+        onLoad={() => setImageLoaded(true)}
       />
-      <button onClick={handleNextImage} className={styles.btn}>
-        다음
-      </button>
+      {imageLoaded && (
+        <button
+          onClick={handleNextImage}
+          className={`${styles.btn} ${styles.SliderBtn} ${styles.right}`}
+        >
+          &gt;
+        </button>
+      )}
+      <div className={styles.indicators}>{renderIndicators()}</div>
     </div>
   );
 };
